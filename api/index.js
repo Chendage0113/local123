9c79dc61bec7a372628eadedd9ebb543c3759643
Chendage0113 <j1136746106@Gmail.com>
2025-03-06 21:51:16 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
-const fs = require('fs');
-
 module.exports = (req, res) => {
   const { lat, long } = req.query;
   const now = new Date().toISOString();
-  const data = `时间: ${now}, 纬度: ${lat}, 经度: ${long}\n`;
-
-  fs.appendFile('/tmp/location.txt', data, (err) => {
-    if (err) {
-      return res.status(500).send('保存失败');
-    }
-    res.status(200).send('位置已记录');
+  res.status(200).json({
+    time: now,
+    latitude: lat,
+    longitude: long,
+    status: "位置接收成功"
   });
 };
 module.exports = (req, res) => {
